4b60eeb5f9be152ff9f3358f956f1503abd3d3be
Sina Amini <general.sina.amini.20@gmail.com>
2022-03-26 15:48:59 +0430

    choose-disease page logic completed

--- src/app/pages/tabs/account/choose-disease/choose-disease.page.ts
+++ src/app/pages/tabs/account/choose-disease/choose-disease.page.ts
@@ -1,4 +1,6 @@
+import { Storage } from '@capacitor/storage';
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-choose-disease',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChooseDiseasePage implements OnInit {
 
-  constructor() { }
+  selectedDiseases = []
+
+  diseases = [
+    {
+      title: 'بیماری های قلبی',
+      checked: false
+    },
+    {
+      title: 'دیابت',
+      checked: false
+    },
+    {
+      title: 'فشار خون',
+      checked: false
+    },
+    {
+      title: 'سایر',
+      checked: false
+    },
+  ]
+
+  constructor(
+    private toastCtrl: ToastController
+  ) { }
 
   ngOnInit() {
+    this.checkSlecetedDiseases();
+  }
+
+  onSelectDisease(disease: string, event: any) {
+    if (event.detail.checked) {
+
+      this.selectedDiseases.push({ title: disease, checked: true })
+    } else {
+      this.selectedDiseases = this.selectedDiseases.filter(d => d.title !== disease)
+    }
+  }
+
+  onSaveDiseases() {
+    Storage.set({ key: 'diseases', value: JSON.stringify(this.selectedDiseases) }).then(() => {
+      this.toastCtrl.create({ message: 'با موفقیت ذخیره شد', duration: 1500, color: 'success' }).then(toastEl => toastEl.present());
+    })
+  }
+
+  checkSlecetedDiseases() {
+    Storage.get({ key: 'diseases' }).then(value => {
+      this.selectedDiseases = JSON.parse(value.value);
+      for (let disease of this.diseases) {
+        if (!(this.selectedDiseases && this.selectedDiseases.length > 0)) {
+          this.selectedDiseases = [];
+          return
+        }
+        if (this.selectedDiseases.filter(d => d.title === disease.title).length > 0) {
+          disease.checked = true
+        }
+      }
+    })
   }
 
 }
