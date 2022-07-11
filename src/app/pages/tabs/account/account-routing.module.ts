d7e3b98e616de41648af99d6a716750f732229f7
Sina <general.sina.amini.20@gmail.com>
2022-07-11 20:18:33 +0430

    refrences page completed

--- src/app/pages/tabs/account/account-routing.module.ts
+++ src/app/pages/tabs/account/account-routing.module.ts
@@ -23,7 +23,11 @@ const routes: Routes = [
   {
     path: 'choose-disease',
     loadChildren: () => import('./choose-disease/choose-disease.module').then( m => m.ChooseDiseasePageModule)
+  },  {
+    path: 'refrences',
+    loadChildren: () => import('./refrences/refrences.module').then( m => m.RefrencesPageModule)
   }
+
 
 ];
 
